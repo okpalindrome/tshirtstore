c198ad92415e576838885970964922f5d33b870f
okpalindrome <Nitinchoudharyn14@gmail.com>
2023-08-24 10:47:40 +0530

    database setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,38 @@ const User = require('../models/user')
 const BigPromise = require('../middlewares/bigPromise')
 const CustomError = require('../utils/customError')
 const cookieToken = require('../utils/cookieToken')
+const fileupload = require('express-fileupload')
+const cloudinary = require('cloudinary')
 
 exports.signup = BigPromise(async (req, res, next) => {
+
+    if(!req.files){
+        return next(new CustomError("Photo is required for signup", 400))
+    }
+
     const {name, email, password} = req.body
 
     if(!name || !email || !password) {
         return next(new CustomError('Name, email and password are requied!', 400))
     }
 
+    let file = req.files.photo
+    
+    const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+        folder: "users",
+        width: 150,
+        crop: "scale"
+    })
+
     // creating a new entry in the DB
     const user = await User.create({
         name, 
         email,
-        password    
+        password,
+        photo: {
+            id: result.public_id,
+            secure_url: result.secure_url
+        }    
     })
 
     cookieToken(user, res)
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const app = require("./app");
 require("dotenv").config();
 const connectWithDb = require('./config/db')
+const cloudinary = require('cloudinary')
 
-//connect with DB
+// connect with DB
 connectWithDb();
+
+// cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port: ${process.env.PORT}`);
