2a3a13faf2b6f35b81106fea3bf488d77b067a88
okpalindrome <Nitinchoudharyn14@gmail.com>
2023-08-26 13:31:43 +0530

    role based controllers

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,13 @@ const {
     passwordReset, 
     getLoggedInUserDetails, 
     changePassword,
-    updateUserDetails} = require('../controllers/userController')
-const { isLoggedIn } = require('../middlewares/user')
+    updateUserDetails,
+    adminAllUser,
+    managerAllUser,
+    adminGetSingleUser, 
+    adminUpdateOneUserDetails,
+    adminDeleteOneUser } = require('../controllers/userController')
+const { isLoggedIn, customerRole } = require('../middlewares/user')
 
 router.route('/signup').post(signup)
 router.route('/login').post(login)
@@ -20,5 +25,16 @@ router.route('/password/reset/:token').post(passwordReset)
 router.route('/userDashboard').get(isLoggedIn, getLoggedInUserDetails)  // isLoggedIn middle is injected in the routes
 router.route('/password/update').post(isLoggedIn, changePassword)
 router.route('/userDashboard/update').post(isLoggedIn, updateUserDetails)
+
+
+// admin routes
+router.route('/admin/users').get(isLoggedIn, customerRole("admin"), adminAllUser)
+router.route('/admin/user/:id')
+.get(isLoggedIn, customerRole("admin"), adminGetSingleUser)
+.put(isLoggedIn, customerRole("admin"), adminUpdateOneUserDetails)
+.delete(isLoggedIn, customerRole("admin"), adminDeleteOneUser)
+
+// manager routes
+router.route('/manager/users').get(isLoggedIn, customerRole("manager"), managerAllUser)
 
 module.exports = router
