b7a119cc3f6b0ddfcee8285b47606c739b479d16
okpalindrome <Nitinchoudharyn14@gmail.com>
2023-08-28 09:21:21 +0530

    more product functions

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { isLoggedIn, customerRole } = require('../middlewares/user')
-const { addProduct, getAllProducts, adminGetAllProducts, getOneProduct } = require('../controllers/productController')
+const { addProduct, 
+    getAllProducts, 
+    adminGetAllProducts, 
+    getOneProduct, 
+    adminUpdateOneProduct, 
+    adminDeleteOneProduct,
+} = require('../controllers/productController')
 
 // user routes
 router.route('/products').get(getAllProducts)
@@ -9,6 +15,10 @@ router.route('/product/:id').get(getOneProduct)
 
 // admin routes
 router.route('/admin/product/add').post(isLoggedIn, customerRole("admin"), addProduct)
-router.route('/admin/products').post(isLoggedIn, customerRole("admin"), adminGetAllProducts)
+router.route('/admin/products').get(isLoggedIn, customerRole("admin"), adminGetAllProducts)
+router.route('/admin/product/:id')
+.put(isLoggedIn, customerRole("admin"), adminUpdateOneProduct)
+.delete(isLoggedIn, customerRole("admin"), adminDeleteOneProduct)
+
 
 module.exports = router
