c250d05bb199c3278d95df0333ae553bdde247ee
okpalindrome <Nitinchoudharyn14@gmail.com>
2023-08-29 06:47:09 +0530

    remaining product controllers and new payment controllers

--- routes/product.js
+++ routes/product.js
@@ -7,18 +7,28 @@ const { addProduct,
     getOneProduct, 
     adminUpdateOneProduct, 
     adminDeleteOneProduct,
+    addReview,
+    deleteReview,
+    getOnlyReviewForOneProduct
 } = require('../controllers/productController')
 
 // user routes
 router.route('/products').get(getAllProducts)
-router.route('/product/:id').get(getOneProduct)
+router.route('/product/reviews')
+  .get(getOnlyReviewForOneProduct) // const productID = req.query
+  .put(isLoggedIn, addReview) // const {rating, comment, productID} = req.body
+  .delete(isLoggedIn, deleteReview) // const productID = req.query
+router.route('/product/:id').get(getOneProduct) // routing with params must be de-prioritised in the sequence
+
+  
+
 
 // admin routes
 router.route('/admin/product/add').post(isLoggedIn, customerRole("admin"), addProduct)
 router.route('/admin/products').get(isLoggedIn, customerRole("admin"), adminGetAllProducts)
 router.route('/admin/product/:id')
-.put(isLoggedIn, customerRole("admin"), adminUpdateOneProduct)
-.delete(isLoggedIn, customerRole("admin"), adminDeleteOneProduct)
+  .put(isLoggedIn, customerRole("admin"), adminUpdateOneProduct)
+  .delete(isLoggedIn, customerRole("admin"), adminDeleteOneProduct)
 
 
 module.exports = router
