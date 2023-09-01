03c77d82c49b84abea2c53ba0f381860cbcdcce0
okpalindrome <Nitinchoudharyn14@gmail.com>
2023-09-01 15:00:43 +0530

    typo

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -2,7 +2,7 @@ const cookieToken = (user, res) => {
         // get session token
         const token = user.getJwtToken()
 
-        // setting teh session token in the cookie
+        // setting the session token in the cookie
         const options = {
             expires: new Date(Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000),
             httpOnly: true
